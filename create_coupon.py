b4cb7256100adfe6fd0ed5b36fd40dcb3651a356
yishii-classmethod-exam <yuki.ishii.classmethod.exam@gmail.com>
2018-10-31 23:00:04 +0900

    define atomic_counts

--- create_coupon.py
+++ create_coupon.py
@@ -1,13 +1,13 @@
-from datetime import datetime
-
-from dynamodb_coupon import dynamodb_put_coupon
+from dynamodb_atomic_counts import dynamodb_atomic_count
+from dynamodb_coupons import dynamodb_put_coupon
 from s3_coupon_image import s3_put_coupon_image
 
 
 def create_coupon(image, image_name):
-    coupon_image_object = s3_put_coupon_image(image, image_name)
+    image_object = s3_put_coupon_image(image, image_name)
+    _id = str(dynamodb_atomic_count('coupon_id'))
     return dynamodb_put_coupon({
-        'id': datetime.now().isoformat(),
-        'image_s3_key': coupon_image_object.key,
+        'id': _id,
+        'image_s3_key': image_object.key,
     })
 
---
+++ dynamodb_atomic_counts.py
@@ -0,0 +1,21 @@
+import functools
+
+import boto3
+
+
+def dynamodb_atomic_count(key):
+    return _dynamodb_atomic_counts_table().update_item(
+        Key={
+            'key': key,
+        },
+        UpdateExpression='set current_number = current_number + :increment',
+        ExpressionAttributeValues={
+            ":increment": 1,
+        },
+        ReturnValues="UPDATED_NEW",
+    )['Attributes']['current_number']
+
+
+@functools.lru_cache()
+def _dynamodb_atomic_counts_table():
+    return boto3.resource('dynamodb').Table('sequences')
